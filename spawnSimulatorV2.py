de0ef96d1d227173d42b6f545c198b7ab4a5a7a3
Rob Steele <robsteele49@gmail.com>
2024-12-29 16:37:55 +0900

    Working on the chromebook with major problems. See the last comment on my last commit.

--- spawnSimulatorV2.py
+++ spawnSimulatorV2.py
@@ -1,9 +1,31 @@
 # spawnSimulatorV2.py
 
+import os
+import platform
 import subprocess
 import threading
 import time
 
+def runCommandNullModem ():
+#    result = subprocess.run(["./nullmodem.sh"],
+#                            capture_output = True,
+#                            text           = True)
+    print ('first line of runCommandNullModem')
+    if platform.system() == 'Windows':
+        result = os.system('./nullmodem.sh')
+    else:
+        result = os.system(f"bash -c ./nullmodem.sh")
+    print (f"result : {result}")
+
+def runCommand (command):
+    print ('first line of runCommand')
+    if platform.system() == 'Windows':
+        result = os.system(command)
+    else:
+        result = os.system(f"bash -c '{command}'")
+    print (f"result : {result}")
+#    return result.stdout, result.returncode
+     
 class runNullModem (threading.Thread):
     
     def __init__ (self, target):
@@ -14,23 +36,30 @@ class runNullModem (threading.Thread):
         self.stopRunNullModem = True
         
     def run(self):
-        pidModem = subprocess.run(['./nullmodem.sh'],
-                                  capture_output = True,
-                                  text           = True)
+        runCommand("./nullmodem.sh")
         while not self.stopRunNullModem:
             print ('inside of runNullModem.run()')
             time.sleep(1)
-
+   
 if __name__ == '__main__':
-    def myLoopingFunction():
-        while True:
-            time.sleep(1)
+#    def myLoopingFunction():
+#        while True:
+#            time.sleep(1)
     
     print ('First line of __main__')
-    
-    threadRunNullModem = runNullModem(target=myLoopingFunction)
-    threadRunNullModem.start()
 
+    runCommandNullModem()
+#    runCommand ("./nullmodem.sh")
+    
+#    threadRunNullModem = runNullModem(target=myLoopingFunction)
+#    threadRunNullModem.start()
+
+    print ('Execute the subprocess.run for nullmodem.sh')
+    
+#    pidModem = subprocess.run(['./nullmodem.sh', '&'],
+#                                capture_output = True,
+#                                text           = True)
+    
     print ('inside of spawnSimulatorV2.__main__')
 
     for i in range (1,10):
@@ -39,7 +68,7 @@ if __name__ == '__main__':
 
     print ('Calling runNullModem.stop')
 
-    threadRunNullModem.stop()
+#    threadRunNullModem.stop()
 
     print ('About to exit from spawnSimulatorV2.__main__')
 
